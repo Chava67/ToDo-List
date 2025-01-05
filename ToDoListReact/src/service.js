7a5ff40b2c341a4d6d0c6ea0620ba8a894ad980a
chz65757 <37327160404@mby.co.il>
2025-01-05 19:28:39 +0200

    URL

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -77,7 +77,8 @@ export default {
   
   login: async (userName, password, navigate) => {
     try {
-      console.log(api.get())
+      console.log(api.baseURL)
+
       const res = await api.post("/login", { userName, password });
       if (res && res.data) {
         saveAccessToken(res.data);  // שמירה על ה-token
